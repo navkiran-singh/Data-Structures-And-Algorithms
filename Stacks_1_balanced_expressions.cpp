77c64ddbca693cfbbdb117cf6e3cc15f37d24d51
Navkiran Singh <43118496+navkiran-singh@users.noreply.github.com>
2019-03-30 16:40:54 +0530

    Implements balanced() without using STL stack.
    Outputs 0 for false, 1 for true

--- Stacks_1_balanced_expressions.cpp
+++ Stacks_1_balanced_expressions.cpp
@@ -12,58 +12,54 @@ class stack_class{
     else cout<<"Stack Overflow\n";
     }
   char pop(){if(top!=-1) return stack[top--];else {cout<<"Stack Underflow\n";return 0;}}
+  char istop(){return stack[top];}
   void display(){while(top!=-1) cout<<"Index:"<<top+1<<" holds "<<pop()<<endl;}
+  bool empty(){if(top==-1) return true; else return false;}
 };
-void push_string_to_stk(stack_class stk, string input){
-    int i=0;
-  while(input[i]!='\0'){
-    stk.push(input[i]);
-    ++i;
-  }
-  stk.display();
-}
-bool balanced(stack_class &stk, string input)
+bool balanced(stack_class &s, string expr)
 {
- int i=0; char element;
- while(input[i]='\0')
- {
-     //if character is an opening type, push it onto stack
-     if(input[i]=='('||input[i]=='{'||input[i]=='[') stk.push(input[i]);
-     
-    
-         //if character of closing type is encountered, pop(from top). If opening-closing pair
-         //is valid, then proceed forward otherwise return false.
-        else {
-            switch(input[i])
-        { 
-            case ')':
-                element=stk.pop();
-                if(element=='{'||element=='[') //not a pair
-                return false;
-                break;
-            
-            case '}':
-                element=stk.pop();
-                if(element=='('||element=='[') return false;
-                break;
-            
-            case ']':
-                element=stk.pop();
-                if(element=='{'||element=='[') return false;
-                break;
-                
-            default:
-                continue;
-        }
-     }
- }
- if(stk.top==-1) return true; else return false;
+	char x; 
 
-}
+	// Traversing the Expression 
+	for (int i=0; i<expr.length(); i++) 
+	{ 
+		if (expr[i]=='('||expr[i]=='['||expr[i]=='{') 
+		{ 
+			// Push the element in the stack 
+			s.push(expr[i]); 
+			continue; 
+		} 
+
+		// IF current current character is not opening 
+		// bracket, then it must be closing. So stack 
+		// cannot be empty at this point. 
+		if (s.empty()) 
+		return false; 
+        x=s.pop();
+		switch (expr[i]) 
+		{ 
+		case ')': 
+			if (x=='{' || x=='[') 
+				return false; 
+			break; 
+
+		case '}': 
+			if (x=='(' || x=='[') 
+				return false; 
+			break; 
+
+		case ']': 
+			if (x =='(' || x == '{') 
+				return false; 
+			break; 
+		} 
+	} 
+
+	// Check Empty Stack 
+	return (s.empty()); }
 int main()
 {stack_class stk;
   string input; cin>>input; 
-  push_string_to_stk(stk,input);
   cout<<balanced(stk,input);
   
 }
